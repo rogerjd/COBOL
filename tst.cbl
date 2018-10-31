27af5e77d143291ada314e5cec3346045662bd3a
roger <rjpd@netzero.com>
2018-10-31 18:50:07 -0400

    first commit

---
+++ tst.cbl
@@ -0,0 +1,22 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       INPUT-OUTPUT SECTION.
+       DATA  DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN.
+      * use paragraph not sections ? MAIN is a paragraph
+       xzz.
+       tst SECTION.
+           DISPLAY "Hello worldxxx".
+           STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
