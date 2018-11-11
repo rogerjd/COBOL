537d499b37bad0fc3ad971efa96e79fd478d99a4
roger <rjpd@netzero.com>
2018-11-11 17:57:47 -0500

    math perform

--- tst.cbl
+++ tst.cbl
@@ -16,11 +16,23 @@
        DATA  DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       77  B PIC 9(3).
+       77  B1 PIC ZZ9.
        PROCEDURE DIVISION.
        MAIN.
       * use paragraph not sections ? MAIN is a paragraph
+           PERFORM ADDTst.
+           PERFORM BBB
+           DISPLAY 'MAIN END'.
+           STOP RUN.
        xzz.
        tst SECTION.
+       BBB.
            DISPLAY "Hello world".
-           STOP RUN.
+
+       math SECTION.
+       ADDTst.
+           ADD 3 to B.
+           MOVE B to B1.
+           DISPLAY B, B1.
        END PROGRAM YOUR-PROGRAM-NAME.
